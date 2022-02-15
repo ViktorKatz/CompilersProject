53a03abf503b792ec6490d24b3e2cf796b1702c7
Viktor <viktorkatz619@gmail.com>
2022-02-15 01:05:49 +0100

    Added start of block comments

---
+++ src/rs/ac/bg/etf/pp1/ast/BlockOfStatementsStart.java
@@ -0,0 +1,57 @@
+// generated with ast extension for cup
+// version 0.8
+// 15/1/2022 0:4:49
+
+
+package rs.ac.bg.etf.pp1.ast;
+
+public class BlockOfStatementsStart implements SyntaxNode {
+
+    private SyntaxNode parent;
+    private int line;
+    public rs.etf.pp1.symboltable.concepts.Obj obj = null;
+
+    public BlockOfStatementsStart () {
+    }
+
+    public SyntaxNode getParent() {
+        return parent;
+    }
+
+    public void setParent(SyntaxNode parent) {
+        this.parent=parent;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public void setLine(int line) {
+        this.line=line;
+    }
+
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+
+    public void childrenAccept(Visitor visitor) {
+    }
+
+    public void traverseTopDown(Visitor visitor) {
+        accept(visitor);
+    }
+
+    public void traverseBottomUp(Visitor visitor) {
+        accept(visitor);
+    }
+
+    public String toString(String tab) {
+        StringBuffer buffer=new StringBuffer();
+        buffer.append(tab);
+        buffer.append("BlockOfStatementsStart(\n");
+
+        buffer.append(tab);
+        buffer.append(") [BlockOfStatementsStart]");
+        return buffer.toString();
+    }
+}
